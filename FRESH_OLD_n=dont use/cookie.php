c1efeda265c9a1102f82552c28eb48753f5f6962
Adedunmi <o.adekoya2@rgu.ac.uk>
2023-03-27 09:32:17 +0100

    Master

--- FRESH_OLD_n=dont use/cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**Created by PhpStorm */
-
-session_start();
-$accesslevel= $_COOKIE['access_level_cookie']
-echo $accesslevel;
-?>
--- FRESH_OLD_n=dont use/materials/bootstrap-5.3.0-alpha1-dist/cookies.php
+++
@@ -1,5 +0,0 @@
-setcookie('travel','flight');
-setcookie('travel', 'flight','parks', 'africa' 'tour','tourism' 'destination', 'trip','/the-tourist-websurf');
-setcookie('travel','tour' 'tourism' 'destination','141760800','', 'www.the-tourist-websurf.com');
-
-if (iss)
